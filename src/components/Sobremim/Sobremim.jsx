b04d796484c51a2205874294a258c885fc1fbc7e
Virgílio César <enjels@outlook.com>
2023-12-13 18:55:02 -0300

    adicionando componente "Sobremim"

---
+++ src/components/Sobremim/Sobremim.jsx
@@ -0,0 +1,22 @@
+import "./Sobremim.css";
+const Sobremim = () => {
+  return (
+    <div className="Sobremim">
+      <img src="" alt="" />
+      <div>
+        <h2>
+          DESENVOLVEDOR <br/> FRONT END.
+        </h2>
+              <p>Sou um dev. Front end apaixonado por transformar ideias em
+                  realidade,combinando habilidades técnicas e criatividade.Estou
+                  sempre em busca de novas oportunidades para aperfeiçoar minhas
+                  habilidade e trabalhar em projetos desafiadores. Meu objetivo é
+                  aprender e crescer na área. Curso <b>Análise e Desenvolvimento de Sistema</b>
+                  pela Unifatecie. Principais tecnologias: <b>Html, Css, JavaScript, React, git, figma e Photoshop</b>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Sobremim;
--- src/components/container_Global/Container.jsx
+++ src/components/container_Global/Container.jsx
@@ -1,12 +1,14 @@
 import "./container.css"
 import Header from "../header/Header";
+import Sobremim from "../Sobremim/Sobremim";
 
 const Container = () => {
     return (
-        <div className="container">
-            <Header/>
-        </div>
-    )
+      <div className="container">
+        <Header />
+        <Sobremim/>
+      </div>
+    );
     
 };
 
