2f5d2e3852e22d3bf69c3baca87dbcd46fa654ba
Virgílio César <enjels@outlook.com>
2023-12-13 17:58:41 -0300

    novo portifólio em react

---
+++ src/components/container_Global/Container.jsx
@@ -0,0 +1,12 @@
+import Header from "../header/Header";
+
+const Container = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+    
+};
+
+export default Container
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+
+import './Header.css'
+function Header() {
+  return (
+    <header>
+      <h1>Virgílio César</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#Projetos">Projetos</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    
+  );
+}
+
+export default Header
